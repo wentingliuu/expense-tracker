70fe505862a0b4740413337a0da38dc56d4f9b5b
wentingliu <cindy810317@gmail.com>
2021-10-29 03:11:19 +0800

    feat: add index.hbs & static files

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const port = 3000
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(express.static('public'))
+
 app.use(routes)
 
 app.listen(port, () => {
