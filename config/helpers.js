09bde865500ff3b25afcf7775fb9cfa21bbf363a
wentingliu <cindy810317@gmail.com>
2021-10-30 00:45:48 +0800

    feat: modify date format

--- config/helpers.js
+++ config/helpers.js
@@ -1,5 +1,14 @@
 module.exports = {
   isSelected: function (a, b) {
     if (a === b) return 'selected'
+  },
+  dateDisplay: function (date) {
+    const yyyy = date.toISOString().slice(0, 4)
+    const mm = date.toISOString().slice(5, 7)
+    const dd = date.toISOString().slice(8, 10)
+    return `${yyyy}/${mm}/${dd}`
+  },
+  isoDateFormat: function (date) {
+    return date.toISOString().slice(0, 10)
   }
 }
