af6c958defdd088566530d74fbcea15021cac108
wentingliu <cindy810317@gmail.com>
2021-10-29 23:38:22 +0800

    feat: add filter by category function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -19,4 +19,22 @@ router.get('/', (req, res) => {
     .catch(error => console.error(error))
 })
 
+router.get('/filter', async (req, res) => {
+  const category = req.query.category
+  const filteredCategory = (category === '全部') ? '' : await Category.findOne({ name: category }).lean()
+  const filter = (filteredCategory === '') ? {} : { categoryId: filteredCategory._id }
+  let totalAmount = 0
+  Record.find(filter)
+    .lean()
+    .then(records => {
+      records.forEach(async record => {
+        totalAmount += Number(record.amount)
+        const recordCategory = await Category.findOne({ _id: record.categoryId }).lean()
+        record.categoryIcon = recordCategory.icon
+      })
+      return res.render('index', { records, totalAmount, category })
+    })
+    .catch(error => console.error(error))
+})
+
 module.exports = router
