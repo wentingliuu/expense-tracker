201107acbb4e5cb83505182abcfc7f1c214191c3
wentingliu <cindy810317@gmail.com>
2021-10-29 18:45:54 +0800

    feat: modify Index.hbs by using handlebars template

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const router = express.Router()
 
+const Record = require('../../models/record')
+const Category = require('../../models/category')
+
 router.get('/', (req, res) => {
-  res.render('index')
+  let totalAmount = 0
+  Record.find()
+    .lean()
+    .then(records => {
+      records.forEach(async record => {
+        totalAmount += Number(record.amount)
+        const recordCategory = await Category.findOne({ _id: record.categoryId }).lean()
+        record.categoryIcon = recordCategory.icon
+      })
+      return res.render('index', { records, totalAmount })
+    })
+    .catch(error => console.error(error))
 })
 
 module.exports = router
