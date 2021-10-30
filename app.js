33db8691799fe5ca09e82a8b7170502435467b55
wentingliu <cindy810317@gmail.com>
2021-10-30 14:10:59 +0800

    feat: add user authentication feature using passport & express-session

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const session = require('express-session')
 const methodOverride = require('method-override')
 
 const routes = require('./routes')
+const usePassport = require('./config/passport')
 require('./config/mongoose')
 
 const app = express()
@@ -10,6 +12,12 @@ const port = 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs', helpers: require('./config/helpers') }))
 app.set('view engine', 'hbs')
+app.use(session({
+  secret: 'RecordSecret',
+  resave: false,
+  saveUninitialized: true
+}))
+usePassport(app)
 
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
