ceaccd20aa8c8b78197d5094899449a584989e01
wentingliu <cindy810317@gmail.com>
2021-10-31 08:01:35 +0800

    feat: add error module & error.hbs

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,12 @@ const home = require('./modules/home')
 const records = require('./modules/records')
 const users = require('./modules/users')
 const auth = require('./modules/auth')
+const error = require('./modules/error')
 
 router.use('/auth', auth)
 router.use('/users', users)
 router.use('/records', authenticator, records)
 router.use('/', authenticator, home)
+router.use('*', error)
 
 module.exports = router
