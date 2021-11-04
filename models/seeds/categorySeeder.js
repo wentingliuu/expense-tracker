a830b2219161b4fd528f647551b55012bb1ffdcb
wentingliu <cindy810317@gmail.com>
2021-11-04 17:17:41 +0800

    fix: modify recordSeeder code

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -3,8 +3,8 @@ const db = require('../../config/mongoose')
 const Category = require('../category')
 const categories = require('./category.json').categories
 
-db.once('open', async () => {
-  await Category.create(categories)
+db.once('open', () => {
+  Category.create(categories)
     .then(() => {
       console.log('categorySeeder created.')
       process.exit()
--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -5,32 +5,32 @@ const Category = require('../category')
 const User = require('../user')
 const Record = require('../record')
 
-const users = require('./user.json').users
-const records = require('./record.json').records
+const userSeeder = require('./user.json').users
+const recordSeeder = require('./record.json').records
 
 db.once('open', async () => {
-  await users.forEach(user => {
-    bcrypt
-      .genSalt(10)
-      .then(salt => bcrypt.hash(user.password, salt))
-      .then(hash => User.create({
-        name: user.name,
-        email: user.email,
-        password: hash
-      }))
-  })
-  await records.forEach(async record => {
+  for (const user of userSeeder) {
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(user.password, salt)
+    await User.create({
+      name: user.name,
+      email: user.email,
+      password: hash
+    })
+  }
+
+  for (const record of recordSeeder) {
     const recordCategory = await Category.findOne({ name: record.category }).lean()
     const recordUser = await User.findOne({ name: record.user }).lean()
-    Record.create({
+    await Record.create({
       name: record.name,
       date: record.date,
       amount: record.amount,
       userId: recordUser._id,
       categoryId: recordCategory._id
     })
-  })
+  }
 
   console.log('recordSeeder created!')
-  console.log('press ^C to quit.')
+  process.exit()
 })
